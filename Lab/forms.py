22503fef897058ebb8e31da9ced484d6068ec395
99Kies <1290017556@qq.com>
2020-02-27 01:16:41 +0800

    add form.py

---
+++ Lab/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length
+from flask_ckeditor import CKEditorField
