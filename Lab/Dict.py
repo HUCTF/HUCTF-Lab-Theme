b371e0146d41b8f143c23127643be4140caf125e
yx <103040077@qq.com>
2020-03-04 17:14:43 +0800

    add Dict.py

---
+++ Lab/Dict.py
@@ -0,0 +1,90 @@
+member_dic={
+    'teacher':{  #类型：老师
+        'teacher_1':{ #第一个老师的信息
+            'img_url':"static/img/cxh.png",#该老师图片的路径
+            'name':'陈晓华',       #该老师的名字
+            'detail_text':'science & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。',
+            'blog':'http://127.0.0.1:8886/',#博客
+        },                     #该老师的详细介绍
+        'teacher_2':{
+            'img_url':"static/img/cxh.png",
+            'name':'陈晓华',
+            'blog':'',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+         'teacher_3':{
+            'img_url':"static/img/cxh.png",
+             'name':'陈晓华',
+            'blog':'',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        }
+    },
+
+    'student_2017':{#类型 学生：2017级，下同
+
+        'student_2017_1':{
+            'img_url':'static/img/jt.jpeg',
+            'name':'杨新',
+            'blog': '',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_2':{
+            'img_url':'static/img/jt.jpeg',
+            'name':'厉程',
+            'blog': '',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_3':{
+            'img_url':'static/img/jt.jpeg',
+            'name':'凌杭',
+            'blog': '',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_4':{
+            'img_url':'static/img/jt.jpeg',
+            'name':'李艺艺',
+            'blog': '',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_5':{
+            'img_url':'static/img/jt.jpeg',
+            'name':'朱鉏红',
+            'blog': '',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_6':{
+            'img_url':'static/img/jt.jpeg',
+            'name':'袁安妮',
+            'blog': '',
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_7': {
+            'img_url': 'static/img/jt.jpeg',
+            'name': '袁安妮',
+            'blog': '',
+            'detail_text': '陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_8': {
+            'img_url': 'static/img/jt.jpeg',
+            'name': '袁安妮',
+            'blog': '',
+            'detail_text': '陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_9': {
+            'img_url': 'static/img/jt.jpeg',
+            'name': '袁安妮',
+            'blog': '',
+            'detail_text': '陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_10': {
+            'img_url': 'static/img/jt.jpeg',
+            'name': '袁安妮',
+            'detail_text': '陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+        'student_2017_11': {
+            'img_url': 'static/img/jt.jpeg',
+            'name': '袁安妮',
+            'detail_text': '陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+        },
+    },
+}
