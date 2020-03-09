3dfcdd1f2d009c6e88e7adf4c0953a7e0e2db338
99Kies <1290017556@qq.com>
2020-03-09 21:51:18 +0800

    update __init__.py

--- Lab/__init__.py
+++ Lab/__init__.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_bootstrap import Bootstrap
-# from flask_moment import Moment
-# from flask_debugtoolbar import DebugToolbarExtension
-
-
-
 
 app = Flask('Lab')
-# app.config.from_pyfile('settings.py')
-# app.config["SECRET_KEY"] = 'hellomyscrect'
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+app.config["SECRET_KEY"] = 'hellomyscrect'
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
-
-
-
-# app.register_blueprint(admin_bp, url_prefix='/admin')
-# app.register_blueprint(user_bp)
-
-
-
-@app.errorhandler(404)
 @app.errorhandler(400)
 def page_not_found(e):
     return render_template('errors/404.html'), 404
@@ -31,15 +14,4 @@ def page_not_found(e):
 def error(e):
     return render_template('errors/500.html'), 500
 
-
-
-
-@app.route('/')
-def index():
-    print(app.config)
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
-    app.run()
-
+from Lab import view, Dict
