a07de8ad5fc3fd5728057626efe01812dfc23440
yx <103040077@qq.com>
2020-03-12 00:48:31 +0800

    update Dict.py

--- Lab/Dict.py
+++ Lab/Dict.py
@@ -1,155 +1,204 @@
+#newstudent.html
+newstudent_dic={
+    '2019':'newstudent_2019.md',
+}
+
 #blog.html
 blog_dic={
-    'blog2':'yx-2.md',
-    'blog1':'yx-1.md',
+    'blog1':{
+        'id':'1',
+        'file_name':'yx-1.md',
+        'title':'css如何在文字超出div元素时自动换行',
+        'author':'杨介帅',
+        'time':'2020/3/7',
+        'type':'前端',
+    },
+    'blog2':{
+        'id':'2',
+        'file_name':'yx-3.md',
+        'title':'c语言中printf的各参数分布',
+        'author':'杨介帅',
+        'time':'2020/3/10',
+        'type':'pwn',
+    },
+    'blog3':{
+        'id':'3',
+        'file_name':'yx-4.md',
+        'title':'格式字符串（摘录）',
+        'author':'杨介帅',
+        'time':'2020/3/10',
+        'type':'pwn',
+    },
+    'blog4':{
+        'id':'4',
+        'file_name':'yx-5.md',
+        'title':'用%n$p来显示栈中任意地址',
+        'author':'杨介帅',
+        'time':'2020/3/10',
+        'type':'pwn',
+    },
+}
+tag_dic={
+    'basic':{
+        'pwn':['2','3','4'],
+        'reverse':[],
+        'misc':[],
+        'web':[],
+        'crypto':[],
+        'other':['1'],
+    },
+    '2017':{
+        '杨介帅':['1','2','3','4'],
+        '厉程':[],
+        '凌杭':[],
+        '李艺艺':[],
+        '朱鉏红':[],
+        '袁安妮':[],
+    },
 }
 
 #history.html
 history_dic={
-    'history_1':{
-        'title':'TITLE1',
-        'time1':'2020/3/6',
-        'time2':'14:00-17:00',
-        'place':'信息工程学院601',
-        'recorder':'杨新',
-        'content':'内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容内容容内容容内容容内容容内容容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容内容容内容容内容容内容容内容容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容内容容内容容内容容内容容内容容内容',
-        'img_dic':{'0':'/static/img/jt.jpeg',
-                    '1':'/static/img/jt.jpeg',
-                    '2':'/static/img/jt.jpeg'},
+    'history_1':{#事件的唯一id
+        'title':'湖州师范学院&浙江海瑞网MIT研究中心成立',#事件标题
+        'time1':'2018/5/12',#事件日期
+        'time2':'18:30-20:00',#具体时间
+        'place':'信息工程学院903',#事件地点
+        'recorder':'杨介帅',#记录者
+                            #记录内容
+        'content':'为了响应习近平总书记在全国网络安全和信息化工作会议上提出的“没有网络安全就没有国家安全”的论断，也为了提高本学院的网络安全竞赛竞争力，于2018年5月，在陈晓华老师与陈晗总经理的带领下，成立了湖州师范学院&浙江海瑞网MIT研究中心,至此，湖州师范学院网络空间安全实验室开始慢慢发展起来。',
+
+        'img_dic':{
+            '0':'/static/img/cl.jpeg',
+      },
     },
     'history_2':{
-        'title':'TITLE2',
-         'time1':'2020/3/6',
-        'time2':'14:00-17:00',
-        'place':'信息工程学院601',
-        'recorder':'杨新',
-        'content':'内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容容内容容内容容内容容内容容内容容内容',
-        'img_dic':{'0':'/static/img/jt.jpeg',
-                    '1':'/static/img/jt.jpeg',
-                    '2':'/static/img/jt.jpeg'},
+        'title':'省赛培训+首届省赛',
+         'time1':'2020/10月-11月',
+        'time2':'8:00-17:00',
+        'place':'杭州电子科技大学校体育馆',
+        'recorder':'杨介帅',
+        'content':'为了备战浙江省首届网络安全竞赛，赛前由郝秀兰老师的带领四名主要成员杨介帅、厉程、凌杭、李艺艺来到杭州进行短期的四天竞赛培训，收获颇丰。最后我们在陈晓华老师与郝秀兰老师的带领下披荆斩棘，经过初赛之后成功进入总决赛，并且成功斩获一个二等奖一个三等级。这两个小小的成绩成为了实验室前进道路的基石。',
+        'img_dic':{'0':'/static/img/sc-7.jpeg',
+                    '1':'/static/img/sc-6.jpeg',
+                    '2':'/static/img/sc-5.jpeg',
+                    '3': '/static/img/sc-4.jpeg',
+                    '4': '/static/img/sc-2.jpeg',
+                    '5': '/static/img/sc-1.jpeg',
+                   },
+    },
+'history_3':{
+        'title':'全国大学生网络安全大赛',
+         'time1':'2019/6/19-2019/6/20',
+        'time2':'8:00-17:00',
+        'place':'苏州',
+        'recorder':'杨介帅',
+        'content':'今年我们容纳了一大批新成员，恰逢全国大学生网络安全大赛开赛，我们共有两支队伍进入线下的分区赛。分区赛中我们遇到了许多强敌，比赛的难度也过高，直至最后我们都没能在ctf阶段获得分数。好在修补阶段一支队伍成功夺分，拿下了赛区三等奖。此次比赛让我们明白了我们与强校的差距，未来的道路仍然任重道远。',
+        'img_dic':{'0':'/static/img/gs-1.jpeg',
+                    '1':'/static/img/gs-2.jpeg',
+                    '2':'/static/img/gs-3.jpeg',
+                    '3': '/static/img/gs-4.jpeg',
+                    '4': '/static/img/gs-5.jpeg',
+                    '5': '/static/img/gs-6.jpeg'
+                   },
+    },
+'history_4':{
+        'title':'浙江省第二届大学生网络与信息安全竞赛',
+         'time1':'2019/9/21',
+        'time2':'8:00-17:00',
+        'place':'杭州电子科技大学校体育馆',
+        'recorder':'杨介帅',
+        'content':'2019年9月21日，由浙江省大学生网络与信息竞赛委员会主办的“浙江省第二届大学生网络与信息安全竞赛”在杭州电子科技大学举行。实验室成员在陈晓华老师的指导下，凭借着扎实的理论知识和努力拼搏的精神，取得了一等奖1项，二等奖3项，三等奖2项的优异成绩，大赛唯一一个“最佳女队奖”也被我们纳入怀中，充分展现了实验室良好的竞技状态和高超的专业技能水平。',
+        'img_dic':{'0':'/static/img/2019sc-1.jpeg',
+                    '1':'/static/img/2019sc-2.jpeg',
+                    '2':'/static/img/2019sc-3.jpeg',
+                    '3': '/static/img/2019sc-4.jpg',
+                    '4': '/static/img/2019sc-5.jpeg',
+                    '5': '/static/img/2019sc-6.jpeg',
+                    '6': '/static/img/2019sc-7.jpeg',
+                   },
     },
 }
 
 #introduction.html
 introduction_dic={
-    'inruduction_text':' 简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介',
+    #简介内容
+    'inruduction_text':' 为了响应习近平总书记在全国网络安全和信息化工作会议上提出的“没有网络安全就没有国家安全”的论断，也为了提高本学院的网络安全竞赛竞争力，于2018年5月，在陈晓华老师与陈晗总经理的带领下，成立了湖州师范学院&浙江海瑞网MIT研究中心；2018年11月，与北京西普阳光教育科技股份有限公司建立网络空间安全联合实验室。我们建立网络空间安全实验室，致力于打造湖州最顶尖的信息安全团队。',
+    'md_dic':{
+        '2019':'2019.md',
+    },
     'img_dic':{
-        '0':{
-          'url': "/static/img/jt.jpeg",
+        '0':{#照片唯一id
+          'url': "/static/img/jt.jpeg",#照片url
           'alt':'first slide' ,
         },
         '1':{
-          'url': "/static/img/jt.jpeg",
+          'url': "/static/img/2019sc-3.jpeg",
           'alt':'second slide' ,
         },
         '3':{
-          'url': "/static/img/jt.jpeg",
+          'url': "/static/img/cl.jpeg",
           'alt': 'third slide',
         },
-        '4':{
-          'url': "/static/img/jt.jpeg",
-          'alt': 'third slide',
-        }
-    }
+        '4': {
+            'url': "/static/img/2019sc-4.jpg",
+            'alt': 'forth slide',
+        },
+
+
+     }
 }
 
 # meber.html
 member_dic={
     'teacher':{  #类型：老师
         'teacher_1':{ #第一个老师的信息
-            'img_url':"static/img/cxh.png",#该老师图片的路径
+            'img_url':"static/img/member_img/cxh.png",#该老师图片的路径
             'name':'陈晓华',       #该老师的名字
-            'detail_text':'science & Remote Sensing》、\n《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。',
-            'blog':'http://127.0.0.1:8886/',#博客
+            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。',
+             'blog':'',#博客
         },                     #该老师的详细介绍
-        'teacher_2':{
-            'img_url':"static/img/cxh.png",
-            'name':'陈晓华',
-            'blog':'',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
-         'teacher_3':{
-            'img_url':"static/img/cxh.png",
-             'name':'陈晓华',
-            'blog':'',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        }
+
     },
 
     'student_2017':{#类型 学生：2017级，下同
         'student_2017_1':{
-            'img_url':'static/img/jt.jpeg',
-            'name':'杨新',
+            'img_url':'static/img/member_img/yjs.png',
+            'name':'杨介帅',
             'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+            'detail_text':'实验室创始人之一，实验室2017级学生负责人，统筹实验室总体工作，奉献于实验室的初期建设。热爱pwn与逆向，虽然能力有限，但仍然执着于此，希望更多的学弟学妹们能将此作为目标，完成我们没有突破的工作。短短大学时光，很荣幸与大家一起见证了实验室从无到有从小到大的过程，我们有低谷也有高光，在我眼里，实验室就是大学里的家，有一种温暖的归属感，希望未来的你们也能继续延续我们的故事，为这个家庭奉献自己的绵薄之力。'
         },
         'student_2017_2':{
-            'img_url':'static/img/jt.jpeg',
+            'img_url':'static/img/member_img/lc.jpg',
             'name':'厉程',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+            'blog': 'https://0clickjacking0.github.io/',
+            'detail_text':'实验室创始人之一，实验室初期的中坚力量，因热衷于安全，转专业到计算机专业，为实验室培养了一大批技术精湛的web手。热爱web渗透，喜欢打ctf，喜欢代码审计，虽然代码审计像cxk，热衷于纯粹的漏洞研究。但希望尽自己的最大努力，使实验室能越走越远，越来越强！'
         },
         'student_2017_3':{
-            'img_url':'static/img/jt.jpeg',
+            'img_url':'static/img/member_img/lh.jpg',
             'name':'凌杭',
             'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+            'detail_text':'实验室创始人之一，实验室初期的中坚力量，校先锋网站长，是实验室初期的杂项高手，为实验室的杂项技术奠定了一定的基础，是实验室在初期时不可缺少的领军人物。从高中开始便有接触计算机领域，特别是对安全领域具有非常浓厚的兴趣，很高兴和大家一起努力创造了网络安全实验室。'
         },
         'student_2017_4':{
-            'img_url':'static/img/jt.jpeg',
+            'img_url':'static/img/member_img/lyy.png',
             'name':'李艺艺',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
+            'blog': 'http://www.lightchriss.top:39000/',
+            'detail_text':'第一位正式的实验室女性成员，推动了实验室密码学方向的发展，是第二届省赛最佳女子队的队长。接触信息安全一年多，学习crypto，misc，也参加过多次CTF夺旗赛。性格上，我是一个比较严谨，稳重的人。但是也经常会与熟悉的朋友打打闹闹。爱好运动打球，但是往往没有时间或没有朋友可以一起运动。希望热爱信息安全的同学，每天都能坚持不懈学习信息安全，科技的发展很快，需要紧跟科技发展的步伐，加油！'
+       },
         'student_2017_5':{
-            'img_url':'static/img/jt.jpeg',
+            'img_url':'static/img/member_img/zch.png',
             'name':'朱鉏红',
             'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
+            'detail_text':'信息工程学院社管会主席，于实验室成立当年加入，最佳女子队队员之一。2018年加入网络空间安全实验室，学习方向为逆向、pwn。我的能力或许不是很突出，但只要有目标就不会轻言放弃。侥幸荣获第十二届全国大学生信息安全竞赛创新实践能力赛赛区三等奖、浙江省第二届大学生网络与信息安全竞赛二等奖。信息的不断发展，带来的是信息安全的不断挑战，我很幸运，感受到了信息安全的魅力。'
         },
         'student_2017_6':{
-            'img_url':'static/img/jt.jpeg',
+            'img_url':'static/img/member_img/yan.png',
             'name':'袁安妮',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
+            'blog': 'https://jsyanni.github.io/',
+            'detail_text':'实验室骨干成员，最佳女子队队员之一。2017届的一个小白，web渗透研究方向，热爱ctf，喜欢和大佬们一起学习和研究技术。(爱好吃火锅，以后大家有机会可以一起去吃火锅。)努力学习技术，希望有一天能追赶上大佬们的脚步。希望大家心中都有自己明确的目标，也希望来到这里的大家可以通过自己的努力，有一些自己在网络安全上独到的见解。'
+         },
     },
-    'student_2018':{#类型 学生：2017级，下同
-        'student_2018_1':{
-            'img_url':'static/img/jt.jpeg',
-            'name':'杨新',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
-        'student_2018_2':{
-            'img_url':'static/img/jt.jpeg',
-            'name':'厉程',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
-        'student_2018_3':{
-            'img_url':'static/img/jt.jpeg',
-            'name':'凌杭',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
-        'student_2018_4':{
-            'img_url':'static/img/jt.jpeg',
-            'name':'李艺艺',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
-        'student_2018_5':{
-            'img_url':'static/img/jt.jpeg',
-            'name':'朱鉏红',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
-        'student_2018_6':{
-            'img_url':'static/img/jt.jpeg',
-            'name':'袁安妮',
-            'blog': '',
-            'detail_text':'陈晓华，博士、副教授、硕士生导师。男，1977年生，2001年本科毕业于江西师范大学，2006年硕士毕业于华东师范大学，2016年获华东师范大学大学工学博士学位。先后主持和参与国家和省部级科研课题4项，获浙江省高校科研成果奖1项和湖州市自然科学优秀论文奖1项。在《IEEE Communications Letters》、《Computer Communications》《IEEE Geoscience & Remote Sensing Letters》、《IEEE Transactions on Geoscience & Remote Sensing》、《软件学报》、《电子学报》等国内外和会议上发表论文20余篇。'
-        },
-    },
+
+
 }
