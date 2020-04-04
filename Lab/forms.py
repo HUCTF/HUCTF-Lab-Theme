ff7fffadb7fed40375ee782a3bf0942653bf889e
yx <1030400677@qq.com>
2020-04-04 22:19:14 +0800

    de

--- Lab/forms.py
+++
@@ -1,4 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length
-from flask_ckeditor import CKEditorField
--- Lab/models.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-from peweb.extensions import db
-from flask_login import UserMixin
